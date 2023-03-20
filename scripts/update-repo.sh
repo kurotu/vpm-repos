f907eddda1d36528b0873c2cbe18e664dc3ccca1
kurotu <kurotu@gmail.com>
2023-03-21 03:08:20 +0900

    Rename to vpm.json

--- scripts/update-repo.sh
+++ scripts/update-repo.sh
@@ -4,6 +4,6 @@ set -eu
 DIRNAME=$(dirname "$0")
 
 cd "$DIRNAME/.."
-./scripts/combine-repos.sh kurotu kurotu https://kurotu.github.io/vpm-repo/index.json \
+./scripts/combine-repos.sh kurotu kurotu https://kurotu.github.io/vpm-repo/vpm.json \
   https://kurotu.github.io/VRCQuestTools/index.json \
-  > index.json
+  > vpm.json
