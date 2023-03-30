af5c371b5bb64081f1dc177d973d1e61b365f060
kurotu <kurotu@gmail.com>
2023-03-30 23:50:54 +0900

    Add id field

--- scripts/combine-repos.sh
+++ scripts/combine-repos.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-# Usage: combine-repos.sh <name> <author> <url> <repo-url1> <repo-url2> ...
+# Usage: combine-repos.sh <name> <id> <author> <url> <repo-url1> <repo-url2> ...
 
 set -eu
 
 NAME="$1"
-AUTHOR="$2"
-URL="$3"
-shift 3
+ID="$2"
+AUTHOR="$3"
+URL="$4"
+shift 4
 
 # downlaod all json files from urls
 PACKAGES=$(for url in "$@"; do
     curl -s "$url" | jq '.packages'
 done | jq -s add)
 
-echo "$PACKAGES" | jq "{name: \"$NAME\", author: \"$AUTHOR\", url: \"$URL\", packages: .}"
+echo "$PACKAGES" | jq "{name: \"$NAME\", id: \"$ID\",author: \"$AUTHOR\", url: \"$URL\", packages: .}"
--- scripts/update-repo.sh
+++ scripts/update-repo.sh
@@ -4,7 +4,7 @@ set -eu
 DIRNAME=$(dirname "$0")
 
 cd "$DIRNAME/.."
-./scripts/combine-repos.sh kurotu kurotu https://kurotu.github.io/vpm-repos/vpm.json \
+./scripts/combine-repos.sh kurotu com.github.kurotu.vpm kurotu https://kurotu.github.io/vpm-repos/vpm.json \
   https://kurotu.github.io/VRCQuestTools/index.json \
   https://kurotu.github.io/MaterialReplacer/index.json \
   > vpm.json
